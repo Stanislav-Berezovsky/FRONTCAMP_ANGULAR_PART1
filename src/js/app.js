4fde6e31762d787846876e2da6a12d43a36bc502
Stanislau Berazouski <stanislau.berazouski@thomsonreuters.com>
2018-03-21 10:38:17 +0300

    add date filter  and possibiliry to change state of toDoItems

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,52 @@
 var toDoListApp = angular.module('toDoListApp', []);
 
-toDoListApp.controller("toDoItemController", function($scope) {
-    var todaysDateAndTime = new Date();
-    $scope.toDoItemsList = [{
+toDoListApp.filter('itemsListDateFilter', function () {
+    return function (items, dayCount) {
+        var compareDate,
+            now = new Date();
+
+        dayCount = dayCount || 0;
+        compareDate = new Date(now.getFullYear(),
+            now.getMonth(), now.getDate() - dayCount);
+
+        return items.filter(function (item) {
+            return compareDate >= item.date;
+        });
+    }
+});
+
+toDoListApp.controller("toDoItemController", function ($scope) {
+    var now = new Date();
+    var toDoItemsList = [
+        {
             description: 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
             isDone: false,
-            date: todaysDateAndTime
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate())
         },
         {
             description: 'ppppppppppppppppppppp',
             isDone: true,
-            date: new Date(todaysDateAndTime.getFullYear(), todaysDateAndTime.getMonth(), todaysDateAndTime.getDate() - 3)
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 3)
         },
         {
             description: 'sssssssssssssssssssssss',
             isDone: true,
-            date: new Date(todaysDateAndTime.getFullYear(), todaysDateAndTime.getMonth(), todaysDateAndTime.getDate() - 5)
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 5)
         },
         {
             description: 'aaaaaaaaaaaaaaaaaaaaaaaa',
             isDone: false,
-            date: todaysDateAndTime
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate())
         },
         {
             description: 'bbbbbbbbbbbbbbbbbbbbbbb',
             isDone: false,
-            date: new Date(todaysDateAndTime.getFullYear(), todaysDateAndTime.getMonth(), todaysDateAndTime.getDate() - 10)
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 10)
         },
         {
             description: 'tttttttttttttttttt',
             isDone: false,
-            date: new Date(todaysDateAndTime.getFullYear(), todaysDateAndTime.getMonth(), todaysDateAndTime.getDate() - 5)
+            date: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 5)
         }
     ];
 
@@ -39,9 +55,26 @@ toDoListApp.controller("toDoItemController", function($scope) {
         doneItemsList: []
     };
 
-    $scope.toDoItemsList.forEach(function(item) {
-       	(item.isDone ? $scope.filteredLists.doneItemsList : 
-       		$scope.filteredLists.processItemsList).push(item);
-    });
+    setFilteredList(toDoItemsList);
 
+    $scope.changeToDoItemsState = function (item, itemIndex) {
+        if (item.isDone) {
+            $scope.filteredLists.processItemsList.splice(itemIndex, 1);
+            $scope.filteredLists.doneItemsList.push(item);
+        } else {
+            $scope.filteredLists.doneItemsList.splice(itemIndex, 1);
+            $scope.filteredLists.processItemsList.push(item);
+        }
+    };
+
+    $scope.fil = function () {
+
+    }
+
+    function setFilteredList(itemsList) {
+        itemsList.forEach(function (item) {
+            (item.isDone ? $scope.filteredLists.doneItemsList :
+                $scope.filteredLists.processItemsList).push(item);
+        });
+    };
 });
